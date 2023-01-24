423dac518b82e60e2394562cd7c2aa8ccfecf7a8
Xaxis <william.neeley@gmail.com>
2023-01-24 10:54:04 -0900

    Added working draggable dots pre touch events

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,36 +1,109 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import useCanvas from './useCanvas'
 
 const Canvas = (props) => {
-    const {...rest} = props
+    const { ...rest } = props
+    const [startDragX, setStartDragX] = useState(0)
+    const [startDragY, setStartDragY] = useState(0)
+    const [curDragX, setCurDragX] = useState(0)
+    const [curDragY, setCurDragY] = useState(0)
+    const [isShapeDragging, setIsShapeDragging] = useState(false)
+    const [activeDraggingShape, setActiveDraggingShape] = useState(null)
+    const [shapes, setShapes] = useState({
+        1: { x: 100, y: 100, radius: 10, color: 'red' },
+        2: { x: 200, y: 200, radius: 10, color: 'green' },
+        3: { x: 300, y: 300, radius: 10, color: 'blue' }
+    })
+    const [shapesArr, setShapesArr] = useState(Object.entries(shapes).map(([key, value]) => value))
 
-    const dots = {
-        red: {x: 20, y: 20, radius: 15, color: 'red'},
-        green: {x: 40, y: 40, radius: 15, color: 'green'},
-        blue: {x: 60, y: 60, radius: 15, color: 'blue'}
+    const handleMouseShapeHitDetect = (mx, my, shape) => {
+        if (shape.radius) {
+            let dx = mx - shape.x
+            let dy = my - shape.y
+            return (dx * dx + dy * dy) < shape.radius * shape.radius
+        }
+        return false
     }
 
-    const shapes = Object.entries(dots).map(([key, value]) => value)
+    const handleMouseTouchDown = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        let mx = e.pageX - e.target.offsetLeft
+        let my = e.pageY - e.target.offsetTop
+        for (let i = 0; i < shapesArr.length; i++) {
+            let shape = shapesArr[i]
+            if (handleMouseShapeHitDetect(mx, my, shape)) {
 
-    const canvasRef = useCanvas({
-        draw: (ctx, frameCount) => {
-            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-            for (let i = 0; i < shapes.length; i++) {
-                let shape = shapes[i]
-                if (shape.radius) {
-                    ctx.beginPath()
-                    ctx.arc(shape.x, shape.y, shape.radius, 0, Math.PI * 2)
-                    ctx.fillStyle = shape.color
-                    ctx.fill()
-                }
+                // Move 'shape' to the end of the array (top of the stack)
+                shapesArr.splice(i, 1)
+                shapesArr.push(shape)
+                setShapesArr(shapesArr)
+
+                // Set the active shape and its coordinates
+                setStartDragX(mx)
+                setStartDragY(my)
+                setIsShapeDragging(true)
+                setActiveDraggingShape(shape)
+                return true
             }
         }
+    }
+
+    const handleMouseTouchUp = (e) => {
+        if (!isShapeDragging) return
+        e.preventDefault()
+        e.stopPropagation()
+        setIsShapeDragging(false)
+        setActiveDraggingShape(null)
+    }
+
+    const handleMouseTouchMove = (e) => {
+        if (!isShapeDragging) return
+        e.preventDefault()
+        e.stopPropagation()
+        let mx = e.pageX - e.target.offsetLeft
+        let my = e.pageY - e.target.offsetTop
+        setCurDragX(mx)
+        setCurDragY(my)
+        let dx = curDragX - activeDraggingShape.x
+        let dy = curDragY - activeDraggingShape.y
+        activeDraggingShape.x += dx
+        activeDraggingShape.y += dy
+        // canvasDraw() // @todo - Should render only happen here?
+        setStartDragX(curDragX)
+        setStartDragY(curDragY)
+    }
+
+    const canvasDraw = (ctx) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        for (let i = 0; i < shapesArr.length; i++) {
+            let shape = shapesArr[i]
+            if (shape.radius) {
+                ctx.beginPath()
+                ctx.arc(shape.x, shape.y, shape.radius + 2, 0, Math.PI * 2)
+                ctx.fillStyle = 'white'
+                ctx.fill()
+                ctx.beginPath()
+                ctx.arc(shape.x, shape.y, shape.radius, 0, Math.PI * 2)
+                ctx.fillStyle = shape.color
+                ctx.fill()
+            }
+        }
+    }
+
+    const canvasRef = useCanvas({
+        draw: canvasDraw,
+        shapes: shapes
     })
 
     return (
         <canvas
             ref={canvasRef}
             {...rest}
+            onMouseDown={handleMouseTouchDown}
+            onMouseUp={handleMouseTouchUp}
+            onMouseOut={handleMouseTouchUp}
+            onMouseMove={handleMouseTouchMove}
         />
     )
 }
--- src/useCanvas.jsx
+++ src/useCanvas.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react'
 
 const useCanvas = (options = {}) => {
-    const { draw } = options
+    const { draw, shapes } = options
     const canvasRef = useRef(null)
 
     const resizeCanvas = (canvas) => {
@@ -20,21 +20,9 @@ const useCanvas = (options = {}) => {
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext(options.context || '2d')
-        let frameCount = 0
-        let animationFrameId
         resizeCanvas(canvas)
-
-        const render = () => {
-            frameCount++
-            draw(context, frameCount)
-            animationFrameId = window.requestAnimationFrame(render)
-        }
-        render()
-
-        return () => {
-            window.cancelAnimationFrame(animationFrameId)
-        }
-    }, [draw])
+        draw(context)
+    }, [draw, shapes])
 
     return canvasRef
 }
