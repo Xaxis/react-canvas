b8dca8f048ce277d0d3125c9805fc50f7e87ed25
Xaxis <william.neeley@gmail.com>
2023-01-26 12:55:29 -0900

    Implemented dot movement bounding box mapped to bg image

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -14,8 +14,8 @@ const CanvasDemo = (props) => {
         initBgColor: '#000000',
         initBgImageSrc: 'https://images.unsplash.com/photo-1501386761578-eac5c94b800a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
         initDots: [1, 2, 3],
-        dotMaxCount: 3,
-        dotRadius: 8
+        dotRadius: 8,
+        drawBgImageBox: false,
     })
 
     useEffect(() => {
--- src/useCanvasDotSelector.jsx
+++ src/useCanvasDotSelector.jsx
@@ -5,33 +5,43 @@ const useCanvasDotSelector = (options = {}) => {
         initBgColor = '#000000',
         initBgImageSrc = '',
         initDots = [],
-        dotMaxCount = 6,
         dotRadius = 10,
+        drawBgImageBox = false,
     } = options
     const canvasRef = useRef(null)
     const [canvasContext, setCanvasContext] = useState(null)
     const [backgroundImage, setBackgroundImage] = useState(null)
-    const [activeShapeCount, setActiveShapeCount] = useState(0)
+    const [bgImageBoundingBox, setBgImageBoundingBox] = useState({
+        tl: { x: 0, y: 0 },
+        tr: { x: 0, y: 0 },
+        bl: { x: 0, y: 0 },
+        br: { x: 0, y: 0 },
+    })
     const [activeShapeKeys, setActiveShapeKeys] = useState(initDots)
     const [activeDraggingShape, setActiveDraggingShape] = useState(null)
     const [defaultShapes, setDefaultShapes] = useState({
-        1: { x: 20, y: 20, radius: dotRadius, color: 'red', added: false },
-        2: { x: 40, y: 40, radius: dotRadius, color: 'green', added: false },
-        3: { x: 60, y: 60, radius: dotRadius, color: 'blue', added: false },
-        4: { x: 80, y: 80, radius: dotRadius, color: 'cyan', added: false },
-        5: { x: 100, y: 100, radius: dotRadius, color: 'magenta', added: false },
-        6: { x: 120, y: 120, radius: dotRadius, color: 'yellow', added: false },
+        1: { x: 20, y: 20, radius: dotRadius, color: 'red' },
+        2: { x: 40, y: 40, radius: dotRadius, color: 'green' },
+        3: { x: 60, y: 60, radius: dotRadius, color: 'blue' },
+        4: { x: 80, y: 80, radius: dotRadius, color: 'cyan' },
+        5: { x: 100, y: 100, radius: dotRadius, color: 'magenta' },
+        6: { x: 120, y: 120, radius: dotRadius, color: 'yellow' },
     })
     const [shapes, setShapes] = useState({})
     const [shapesArr, setShapesArr] = useState([])
 
-    const handleMouseShapeHitDetect = (mx, my, shape) => {
+    const handleMouseTouchShapeHitDetect = (mx, my, shape) => {
         if (shape.radius) {
             let dx = mx - shape.x
             let dy = my - shape.y
             return (dx * dx + dy * dy) < shape.radius * shape.radius
         }
         return false
+    }
+
+    const handleMouseTouchInsideBgImageBoundingBoxHitDetect = (mx, my) => {
+        const { tl, tr, bl } = bgImageBoundingBox
+        return (mx >= tl.x + dotRadius && mx <= tr.x - dotRadius && my >= tl.y + dotRadius && my <= bl.y - dotRadius)
     }
 
     const handleMouseTouchDown = (e) => {
@@ -47,7 +57,7 @@ const useCanvasDotSelector = (options = {}) => {
         }
         for (let i = 0; i < shapesArr.length; i++) {
             let shape = shapesArr[i]
-            if (handleMouseShapeHitDetect(mx, my, shape)) {
+            if (handleMouseTouchShapeHitDetect(mx, my, shape)) {
                 canvasRef.current.style.cursor = 'move'
 
                 // Move 'shape' to the end of the array (top of the stack)
@@ -79,7 +89,7 @@ const useCanvasDotSelector = (options = {}) => {
             my = mmy = e.pageY - e.target.offsetTop
             for (let i = 0; i < shapesArr.length; i++) {
                 let shape = shapesArr[i]
-                if (handleMouseShapeHitDetect(mmx, mmy, shape)) {
+                if (handleMouseTouchShapeHitDetect(mmx, mmy, shape)) {
                     canvasRef.current.style.cursor = 'move'
                     break
                 } else {
@@ -88,11 +98,14 @@ const useCanvasDotSelector = (options = {}) => {
             }
         }
         if (!activeDraggingShape) return
-        let dx = mx - activeDraggingShape.x
-        let dy = my - activeDraggingShape.y
-        drawCanvas()
-        activeDraggingShape.x += dx
-        activeDraggingShape.y += dy
+        const isMoveWithinBounds = handleMouseTouchInsideBgImageBoundingBoxHitDetect(mx, my)
+        if (isMoveWithinBounds) {
+            let dx = mx - activeDraggingShape.x
+            let dy = my - activeDraggingShape.y
+            drawCanvas()
+            activeDraggingShape.x += dx
+            activeDraggingShape.y += dy
+        }
     }
 
     const loadImageIntoCanvas = (imageSrc) => {
@@ -117,27 +130,53 @@ const useCanvasDotSelector = (options = {}) => {
     const drawCanvas = (options = {}) => {
         const { shapesOverride } = options
         let shapesArrInit = shapesArr
-        if (shapesOverride) {
-            shapesArrInit = Object.entries(shapesOverride).map(([key, value]) => value)
-            setShapesArr(shapesArrInit)
-        }
         if (canvasContext) {
+            const { devicePixelRatio:ratio=1 } = window
+            const canvasElmWidth = canvasContext.canvas.width / ratio
+            const canvasElmHeight = canvasContext.canvas.height / ratio
+
+            // Scale image to maintain its aspect ratio with its height equal to the canvas height
+            const aspectRatio = backgroundImage.width / backgroundImage.height
+            const newImgWidth = canvasElmHeight * aspectRatio
+            const newImgHeight = canvasElmHeight
+            backgroundImage.width = newImgWidth
+            backgroundImage.height = newImgHeight
+
+            // Get coordinates to center the image inside the canvas
+            const x = (canvasElmWidth - newImgWidth) / 2
+
+            // Update background image coordinates
+            setBgImageBoundingBox({
+                tl: { x, y: 0 },
+                tr: { x: x + newImgWidth, y: 0 },
+                bl: { x, y: newImgHeight },
+                br: { x: x + newImgWidth, y: newImgHeight },
+            })
+
+            // Use passed shapes parameter to redraw canvas
+            if (shapesOverride) {
+                shapesArrInit = Object.entries(shapesOverride).map(([key, shape]) => {
+                    return { ...shape, x: shape.x + x, y: shape.y }
+                })
+                setShapesArr(shapesArrInit)
+            }
+
+            // Clear canvas
             canvasContext.clearRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height)
-            if (backgroundImage) {
-                const { devicePixelRatio:ratio=1 } = window
-                const canvasElmWidth = canvasContext.canvas.width / ratio
-                const canvasElmHeight = canvasContext.canvas.height / ratio
 
-                // Scale image to maintain its aspect ratio with its height equal to the canvas height
-                const aspectRatio = backgroundImage.width / backgroundImage.height
-                const newWidth = canvasElmHeight * aspectRatio
-                const newHeight = canvasElmHeight
-                backgroundImage.width = newWidth
-                backgroundImage.height = newHeight
+            // Draw background image
+            canvasContext.drawImage(backgroundImage, x, 0, newImgWidth, newImgHeight)
 
-                // Get coordinates to center the image inside the canvas
-                const x = (canvasElmWidth - newWidth) / 2
-                canvasContext.drawImage(backgroundImage, x, 0, newWidth, newHeight)
+            // Draw image bounding box for debugging
+            if (drawBgImageBox) {
+                canvasContext.beginPath()
+                canvasContext.moveTo(x, 0)
+                canvasContext.lineTo(x + newImgWidth, 0)
+                canvasContext.lineTo(x + newImgWidth, newImgHeight)
+                canvasContext.lineTo(x, newImgHeight)
+                canvasContext.lineTo(x, 0)
+                canvasContext.strokeStyle = 'red'
+                canvasContext.stroke()
             }
             for (let i = 0; i < shapesArrInit.length; i++) {
                 let shape = shapesArrInit[i]
@@ -156,6 +195,23 @@ const useCanvasDotSelector = (options = {}) => {
     }
 
     useEffect(() => {
+
+        // Set default styles
+        canvasRef.current.style.touchAction = 'none'
+        canvasRef.current.style.backgroundColor = initBgColor
+
+        // Load initial background image
+        if (initBgImageSrc) loadImageIntoCanvas(initBgImageSrc)
+
+        // @todo: draw initial shapes within background image bounding box
+        // Object.entries(shapes).map(([key, shape]) => {
+        //     shapes[key] = { ...shape }
+        //     shapes[key].x = shape.x + bgImageBoundingBox.tl.x
+        //     shapes[key].y = shape.y + bgImageBoundingBox.tl.y
+        // })
+        // console.log(shapes)
+
+        // Add initial shapes
         if (activeShapeKeys && activeShapeKeys.length) {
             const newShapes = activeShapeKeys.reduce((acc, key) => {
                 if (defaultShapes[key]) {
@@ -173,27 +229,20 @@ const useCanvasDotSelector = (options = {}) => {
     }, [activeShapeKeys])
 
     useEffect(() => {
-        canvasRef.current.style.touchAction = 'none'
-        canvasRef.current.style.backgroundColor = initBgColor
-        if (initBgImageSrc) loadImageIntoCanvas(initBgImageSrc)
-    }, [canvasRef])
 
-    useEffect(() => {
+        // Initialize canvas and device pixel density
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         setCanvasContext(context)
         if (canvasContext) {
             resizeCanvas(canvas)
-            drawCanvas()
-
         }
-    }, [canvasContext, shapes])
 
-    useEffect(() => {
+        // Initialize shapes
         if (Object.entries(shapes).length) {
             drawCanvas({ shapesOverride: shapes })
         }
-    }, [shapes])
+    }, [canvasContext, shapes])
 
     return {
         canvasRef,
